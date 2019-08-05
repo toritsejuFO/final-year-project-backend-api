4e43bb986db5cd85d353b1662fb8d0c2664ed159
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-05 10:52:45 +0100

    Update: Log request/reponse details after every request

--- run_api.py
+++ run_api.py
@@ -2,10 +2,13 @@ import os
 import sys
 import unittest
 
+import arrow
+from flask import request
 from dotenv import load_dotenv
 from flask_migrate import Migrate
 
 from api import create_app, db
+from logger import request_logger
 
 load_dotenv()
 
@@ -19,6 +22,10 @@ app.app_context().push()
 migrate = Migrate(app, db)
 
 @app.cli.command()
+def run():
+    app.run()
+
+@app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('test', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
@@ -26,5 +33,21 @@ def test():
         return True
     else:
         return False
+
+@app.after_request
+def log_info(response):
+    date = arrow.now('Africa/Lagos')
+    log_details = {
+        'date': str(date),
+        'user_ip': request.remote_addr,
+        'browser': request.user_agent.browser,
+        'user_device': request.user_agent.platform,
+        'method': request.method,
+        'request_url': request.url,
+        'status_code': response.status_code,
+    }
+    request_logger.warning(log_details)
+    app.logger.info(log_details)
+    return response
 
 from utils import cli_utils
