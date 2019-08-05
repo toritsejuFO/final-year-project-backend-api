0ddcde96f2e31041c676af03fa4a40fd0e33b886
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-05 10:50:54 +0100

    Setup logging configuration

---
+++ logger.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+
+file_handler = RotatingFileHandler(
+    'logs/api.log', maxBytes=10240, backupCount=10)
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(logging.Formatter(
+    '[%(asctime)s] %(levelname)s: %(message)s - [%(pathname)s:%(lineno)s]'))
+
+# To log request only after every request
+request_logger = logging.getLogger('request_logger')
+request_handler = RotatingFileHandler(
+    'logs/request.log', maxBytes=10240, backupCount=10)
+request_handler.setLevel(logging.INFO)
+request_handler.setFormatter(logging.Formatter('[%(asctime)s] - %(message)s'))
+request_logger.addHandler(request_handler)
