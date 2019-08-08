b97cbad85d91d12fbd6e3a96124b7ced66fa2098
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-08 12:26:09 +0100

    Implement Student's details update

--- api/service/students_service.py
+++ api/service/students_service.py
@@ -1,4 +1,4 @@
-from api.model import Student
+from api.model import Student, Department, Level
 
 class StudentService():
     @staticmethod
@@ -46,6 +46,47 @@ class StudentService():
             response['message'] = 'Internal Server Error'
             return response, 500
 
+        if not student:
+            response['status'] = False
+            response['message'] = 'Student does not exist anymore'
+            return response, 400
+
         response['status'] = True
         response['data'] = student.to_dict
+        return response, 200
+
+    @staticmethod
+    def edit_me(reg_no, data):
+        response = {}
+        level = data['level']
+        department = data['department']
+        try:
+            student = Student.query.filter_by(reg_no=reg_no).first()
+        except:
+            response['status'] = False
+            response['message'] = 'Internal Server Error'
+            return response, 500
+
+        if not student:
+            response['status'] = False
+            response['message'] = 'Student does not exist anymore'
+            return response, 400
+
+        if student.reg_complete:
+            response['status'] = False
+            response['message'] = "You cannot update your details anymore"
+            return response, 403
+
+        try:
+            student.level = Level.query.filter_by(level=level).first()
+            student.department = Department.query.filter_by(code=department).first()
+            student.reg_complete = True
+            student.save()
+        except:
+            response['status'] = False
+            response['message'] = 'Internal Server Error'
+            return response, 500
+
+        response['status'] = True
+        response['message'] = 'Details updated successfully'
         return response, 200
