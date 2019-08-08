32dc95e3d1dfd44b12087ffda9d32ff43af62ed4
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-08 17:38:44 +0100

    Implement endpoint to view courses via department and level

--- api/controller/courses_controller.py
+++ api/controller/courses_controller.py
@@ -2,11 +2,18 @@ from flask_restplus import Resource, Namespace
 
 from api.service import CourseService
 
-course_api = Namespace('courses', description='API endpoints for manaigin courses resourse')
+course_api = Namespace('courses', description='API endpoints for managing courses resourse')
 
 @course_api.route('')
 class CourseList(Resource):
     @course_api.doc('Get All Courses')
     def get(self):
         response, code = CourseService.get_all_courses()
+        return response, code
+
+@course_api.route('/<string:department>/<string:level>')
+class Course(Resource):
+    @course_api.doc('Get Courses via Department & Level')
+    def get(self, department, level):
+        response, code = CourseService.get_courses_by_dept_level(department=department, level=level)
         return response, code
--- api/service/courses_service.py
+++ api/service/courses_service.py
@@ -1,4 +1,4 @@
-from api.model import Course
+from api.model import Course, Department, Level
 
 class CourseService:
     @staticmethod
@@ -10,6 +10,32 @@ class CourseService:
             response['status'] = False
             response['message'] = 'Internal Server Error'
             return response, 500
+
+        if not courses:
+            response['status'] = False
+            response['message'] = 'No courses found'
+            return response, 404
+        
+        response['status'] = True
+        response['data'] = [course.to_dict for course in courses]
+        return response, 200
+
+    @staticmethod
+    def get_courses_by_dept_level(department, level):
+        response = {}
+        try:
+            department_ = Department.query.filter_by(code=department).first()
+            level_ = Level.query.filter_by(level=level).first()
+            courses = Course.query.filter_by(department=department_, level=level_).all()
+        except:
+            response['status'] = False
+            response['message'] = 'Internal Server Error'
+            return response, 500
+
+        if not courses:
+            response['status'] = False
+            response['message'] = f'No courses found for "{department}" department in "{level}" level'
+            return response, 404
 
         response['status'] = True
         response['data'] = [course.to_dict for course in courses]
