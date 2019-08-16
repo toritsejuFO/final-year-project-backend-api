f4dc7ac7e1d888b83d1922df43aad0ffaeae19f1
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-16 15:03:09 +0100

    Include HOD namespace to swagger UI docs

--- api/__init__.py
+++ api/__init__.py
@@ -32,10 +32,12 @@ def create_app(config_name):
     from api.controller import course_api as course_ns
     from api.controller import lecturer_api as lecturer_ns
     from api.controller import lecturer_auth_api as lecturer_auth_ns
+    from api.controller import hod_api as hod_ns
     api.add_namespace(student_ns, path='/students')
     api.add_namespace(student_auth_ns, path='/students')
     api.add_namespace(course_ns, path='/courses')
     api.add_namespace(lecturer_ns, path='/lecturers')
     api.add_namespace(lecturer_auth_ns, path='/lecturers')
+    api.add_namespace(hod_ns, path='/hods')
 
     return app
