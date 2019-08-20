59482242afc2f25ca94914d38516a5f61385924b
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-20 21:32:39 +0100

    Implement schema validation for HOD update details

--- api/schema/hods_schema.py
+++ api/schema/hods_schema.py
@@ -11,6 +11,46 @@ NewHOD = namedtuple('NewHOD', [
     'password'
 ])
 
+EditHOD = namedtuple('EditHOD', [
+    'name',
+    'email',
+    'password',
+])
+
+class EditHODSchema(Schema):
+    name = fields.String(required=True, error_messages={'required': 'Name is required'})
+    email = fields.Email(required=True, error_messages={'required': 'Email is required'})
+    password = fields.String(required=True, error_messages={'required': 'Password is required'})
+
+    @post_load
+    def edit_me(self, data):
+        return EditHOD(**data)
+
+    @validates('name')
+    def validate_name(self, value):
+        max_len = 100
+        if not value:
+            raise ValidationError('Name cannot be empty')
+        if len(value) > max_len:
+            raise ValidationError(f'Name cannot exceed {max_len} characters')
+
+    @validates('email')
+    def Validate_email(self, value):
+        max_len = 128
+        hod = HOD.query.filter_by(email=value).first()
+        if not value:
+            raise ValidationError('Email cannot be empty')
+        if len(value) > max_len:
+            raise ValidationError(f'Email cannot exceed {max_len} characters')
+        if hod:
+            raise ValidationError('HODs with this email already exists')
+    
+    @validates('password')
+    def validate_password(self, value):
+        if not value:
+            raise ValidationError('Password cannot be empty')
+
+
 class NewHODSchema(Schema):
     name = fields.String(required=True, error_messages={'required': 'Name is required'})
     email = fields.Email(required=True, error_messages={'required': 'Email is required'})
