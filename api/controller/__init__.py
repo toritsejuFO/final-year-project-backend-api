0d709b34ae23d1365d69af506d5a222de57ea4a3
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-26 14:13:10 +0100

    Expose current semester/session endpoint
    Return all departments in a sorted order

--- api/controller/__init__.py
+++ api/controller/__init__.py
@@ -1,6 +1,6 @@
 from api.controller.students_controller import student_api
 from api.controller.auth_controller import student_auth_api, lecturer_auth_api, hod_auth_api, auth_verification_api
-from api.controller.courses_controller import course_api
+from api.controller.courses_controller import course_api, current_api
 from api.controller.lecturers_controller import lecturer_api
 from api.controller.hods_controller import hod_api
 from api.controller.departments_controller import dept_api
--- api/controller/courses_controller.py
+++ api/controller/courses_controller.py
@@ -1,8 +1,15 @@
+import os
+
 from flask_restplus import Resource, Namespace
 
 from api.service import CourseService
 
-course_api = Namespace('courses', description='API endpoints for managing courses resourse')
+course_api = Namespace(
+    'courses', description='API endpoints for managing courses resourse')
+
+current_api = Namespace(
+    'current', description='API endpoints for getting current session and semester')
+
 
 @course_api.route('')
 class CourseList(Resource):
@@ -11,9 +18,33 @@ class CourseList(Resource):
         response, code = CourseService.get_all_courses()
         return response, code
 
+
 @course_api.route('/<string:department>/<string:level>')
 class Course(Resource):
     @course_api.doc('Get Courses via Department & Level')
     def get(self, department, level):
-        response, code = CourseService.get_courses_by_dept_level(department=department, level=level)
+        response, code = CourseService.get_courses_by_dept_level(
+            department=department, level=level)
         return response, code
+
+
+@current_api.route('/semester')
+class CurrentSemester(Resource):
+    @current_api.doc('Get current semester')
+    def get(self):
+        response = {
+            'success': True,
+            'data': os.environ.get('CURRENT_SEMESTER')
+        }
+        return response, 200
+
+
+@current_api.route('/session')
+class CurrentSession(Resource):
+    @current_api.doc('Get current session')
+    def get(self):
+        response = {
+            'success': True,
+            'data': os.environ.get('CURRENT_SESSION')
+        }
+        return response, 200
