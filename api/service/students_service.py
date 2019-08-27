e5d8464ef53cd667b00626e19a89abc0522e28e5
Faith Odonghanro <toriboi.fo@gmail.com>
2019-08-27 12:36:26 +0100

    Fix: Student can register courses without thumprinting and registering courses

--- api/service/students_service.py
+++ api/service/students_service.py
@@ -108,9 +108,21 @@ class StudentService():
             return response, 404
 
         # Select courses based on student's level and current semester
-        courses = list(filter(
-            lambda c: c.semester.semester == semester and c.level.level == student.level.level,
-            student.department.courses))
+        # If and only if student has completed registration
+        if not student.reg_complete or not student.fingerprint_template:
+            response['success'] = False
+            response['message'] = 'Student registration is incomplete and thus can not access courses'
+            return response, 423
+
+        try:
+            courses = list(filter(
+                lambda c: c.semester.semester == semester and c.level.level == student.level.level,
+                student.department.courses))
+        except Exception:
+            response['success'] = False
+            response['message'] = 'Internal Server Error'
+            return response, 500
+        
 
         response['success'] = True
         response['data'] = [course.to_dict for course in courses]
@@ -130,6 +142,11 @@ class StudentService():
             response['success'] = False
             response['message'] = 'Student Not Found'
             return response, 404
+
+        if not student.reg_complete or not student.fingerprint_template:
+            response['success'] = False
+            response['message'] = 'You must complete your registration and thumbprint before registering your courses'
+            return response, 423
 
         if student.has_registered_course:
             response['success'] = False
