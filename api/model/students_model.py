5ac96673025400d12bbd88111f7dca0b5ed6aa05
Faith Odonghanro <toriboi.fo@gmail.com>
2019-11-11 00:52:43 +0100

    Implement model and table for student lecture attendance

--- api/model/students_model.py
+++ api/model/students_model.py
@@ -11,6 +11,8 @@ session = os.environ.get('CURRENT_REGISTERED_COURSES_SESSION')
 semester = os.environ.get('CURRENT_REGISTERED_COURSES_SEMESTER')
 registered_courses_table_name = select_table_name(f'REGISTERED_COURSES_{semester}_{session}')
 exam_table_name = select_table_name(f'STUDENTS_EXAM_{semester}_{session}')
+student_lectures_table_name = select_table_name(f'STUDENT_LECTURES_{semester}_{session}')
+lecturer_lectures_table_name = select_table_name(f'LECTURER_LECTURES_{semester}_{session}')
 
 registered_courses = db.Table(registered_courses_table_name,
     db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
@@ -19,6 +21,11 @@ registered_courses = db.Table(registered_courses_table_name,
 exam_attendance = db.Table(exam_table_name,
     db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
     db.Column('course_id', db.Integer, db.ForeignKey('courses.id'), primary_key=True))
+
+lecture_attendance = db.Table(student_lectures_table_name,
+    db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'), primary_key=True),
+    db.Column('count', db.Integer, default=0))
 
 
 class Student(db.Model):
@@ -41,6 +48,8 @@ class Student(db.Model):
         'students_registered', lazy='dynamic'), lazy='dynamic')
     exam_attendance = db.relationship('Course', secondary=exam_attendance, backref=db.backref(
         'students_exam_attended', lazy='dynamic'), lazy='dynamic')
+    lecture_attendance = db.relationship('Course', secondary=lecture_attendance,
+        backref=db.backref('student_lecture_attended', lazy='dynamic'), lazy='dynamic')
     has_registered_course = db.Column(db.Boolean, default=False)
 
     def __init__(self, firstname=None, lastname=None, othername=None, reg_no=None, email=None, password=None):
@@ -84,6 +93,46 @@ class Student(db.Model):
 
     def exam_attendance_taken(self, course):
         return self.exam_attendance.filter(exam_attendance.c.course_id == course.id).count() > 0
+
+    def attend_lecture(self, course, lecturer):
+        if not self.is_lecture_attended(course):
+            self.lecture_attendance.append(course)
+            db.session.commit()
+
+        # Get current lecturer lecture count
+        sql = f'''
+            SELECT count
+            FROM {lecturer_lectures_table_name}
+            WHERE lecturer_id = {lecturer.id}
+            AND course_id = {course.id}
+            LIMIT 1
+        '''
+        result = db.session.execute(sql)
+        lecturer_lecture_count = result.fetchone()[0]
+        print(lecturer_lecture_count)
+
+        # Check if attendance has been taken by student
+        sql = f'''
+            SELECT day{lecturer_lecture_count}
+            FROM {student_lectures_table_name}
+            WHERE student_id = {self.id}
+            AND course_id = {course.id}
+            LIMIT 1
+        '''
+        result = db.session.execute(sql)
+        student_lecture_count_for_day = result.fetchone()[0]
+
+        if student_lecture_count_for_day == 0:
+            sql = f'''
+                UPDATE {student_lectures_table_name}
+                SET day{lecturer_lecture_count} = 1, count = count + 1
+                WHERE student_id = {self.id}
+                AND course_id = {course.id}
+            '''
+            result = db.session.execute(sql)
+
+    def is_lecture_attended(self, course):
+        return self.lecture_attendance.filter(lecture_attendance.c.course_id == course.id).count() > 0
 
     @property
     def get_level(self):
